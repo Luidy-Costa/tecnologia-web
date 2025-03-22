113c45cd4ee4a220f86aad15eb044983f4163539
Luidy-Costa <lluidycosta2024@gmail.com>
2025-03-21 22:56:28 -0300

    dicas e estrategias
    criei e estilizei as paginas de dicas e estrategias

--- DOM/script.js
+++ DOM/script.js
@@ -1,6 +1,25 @@
-document.querySelector("#querySelectorChangeColor").addEventListener("click", function() { 
+//QuerySelector
 
-    this.style.background = "pink";
+document.querySelector("#querySelectorChangeColor").addEventListener("click", function(){ this.style.backgroundColor = "lighblue"; 
 
 });
 
+document.querySelectorAll(".box").forEach(box => {
+    box.addEventListener("click", function(){
+        this.style.backgroundColor = "lightcoral";
+    });
+});
+
+window.onload = function(){
+ alert("Sua página foi carregada com sucesso!")
+};
+
+document.getElementById("scrollToTop").addEventListener("click", function(){
+    window.scrollTo({top: 0, behavior: "smooth"});
+})
+
+document.getElementById("eventListenerBox").addEventListener("mouseover", function(){
+    this.textContent = "O mouse passou por aqui :)"
+});
+
+document.getElementById("createElementBtn").
